3c95d277909f7695dcd244e4233014ea85715093
juanrocha <aguilajk@gmail.com>
2020-12-18 13:22:14 +0100

    major update on manuscript with co-authors input. New figures.

--- 03-figueres.R
+++ 03-figueres.R
@@ -3,6 +3,8 @@ library(tidyverse)
 library(tm)
 library (topicmodels)
 library(lda)
+library(patchwork)
+
 
 load("data/models_gibbs.RData")
 
@@ -36,11 +38,12 @@ perp$Gibbs <- NA
 # pretty names:
 df_stats$model <- c("VEM alpha", "VEM fixed", "Gibbs", "CTM")
 
-df_stats %>%
+g1 <- df_stats %>%
     add_column(perplexity = as.numeric(perp[c(1,2,4,3)])) %>% #minimize perplexity
     pivot_longer(cols = 2:4, names_to = "measure", values_to = "value") %>%
     ggplot(aes(model, value)) + 
     geom_col() + facet_wrap(.~measure, scales = "free") +
+    labs(x = "Algorithm", y = "Value", tag = "A") +
     theme_light(base_size = 6) 
 
 # ggsave(filename = "figures/fig1_algorithm_selection.png",
@@ -49,24 +52,26 @@ df_stats %>%
 # J201117: the best algo is Gibbs sampling!
 
 #### number of topics ####
-df_topic_number %>%
+g2 <- df_topic_number %>%
     # mutate(alpha_log = log10(alpha)) %>%
     pivot_longer(cols = 2:last_col(), names_to = "measure", values_to = "value") %>%
     # filter(measure != "alpha") %>%
     ggplot(aes(as.factor(topic_number), value)) +
     geom_col() + 
-    labs(x = "Number of topics") +
+    labs(x = "Number of topics", y = "Value", tag = "B") +
     facet_wrap(.~measure, scales = "free", ncol = 4, nrow = 1) +
     theme_light(base_size = 6)
 
-# ggsave(filename = "figures/fig2_topicnumber.png",
-#        device = "png", width = 5, height = 2)
+g1/g2
+
+ggsave(filename = "paper/figures/fig3_topicnumber.png",
+       device = "png", width = 5, height = 4)
 
 
 #### heatmaps topics ####
 
 gplots::heatmap.2(
-    posterior(topicNumber.TM[[2]])$topics, 
+    posterior(topicNumber.TM[[3]])$topics, 
     key=T, trace="none", margins=c(5,5), 
     cexRow=0.7, cexCol=0.7, col="topo.colors", keysize=1, key.par = list(cex = 0.5),
     labRow = FALSE, key.title = NA, key.xlab = "probability", key.ylab = "count" )
@@ -77,7 +82,7 @@ df_topics10 <- tidy(topicNumber.TM[[2]], matrix = "beta")
 df_topics25 <- tidy(topicNumber.TM[[3]], matrix = "beta")
 
 ## visualization of topics
-g1 <- df_topics25 %>%
+g_25 <- df_topics25 %>%
     group_by(topic) %>%
     top_n(10, beta) %>% 
     ungroup() %>%
@@ -87,10 +92,13 @@ g1 <- df_topics25 %>%
     geom_col(aes(fill = as.factor(topic)),show.legend = FALSE) +
     coord_flip() + 
     scale_x_reordered() +
-    # labs(tag = "A") +
+    labs(tag = "D", y = "Probability of word explaining the topic", x = "Word ranking") +
     facet_wrap(.~ topic, scales = "free_y", ncol = 5) +
     theme_light(base_size = 6) + 
-    theme(axis.text.x = element_text(size = 3))
+    theme(axis.text.x = element_text(size = 5))
+
+
+
 
 ggsave("fig3_topics25_time.png", device = "png", width = unit(5,"in"), height = unit(5,"in"))
 
@@ -100,33 +108,41 @@ df_documents <- tidy(topicNumber.TM[[3]], matrix = "gamma")
 
 ## read data agian from 01-read_data.R until line 21
 
-g <- df_documents %>% 
-    left_join(
-        dat %>% 
-            select(abstract, title, year) %>%
-            filter(!is.na(abstract)) %>%
-            unique(),
-        by = c("document" = "title") ) %>%
-    mutate(year = as.numeric(year)) %>%
-    filter(year > 2000) %>%
-    ggplot(aes(year, gamma) )+
-    geom_jitter(
-        aes(color = as.factor(topic), fill = as.factor(topic), alpha = gamma),
-        size = 1, show.legend = FALSE) +
-    geom_smooth(method = "loess") +
-    scale_alpha_continuous(range = c(0.1,1)) +
-    facet_wrap(.~topic, ncol = 5) +
-    #labs(tag = "B") +
-    theme_light(base_size = 6) + 
-    theme(axis.text.x = element_text(size = 4))
+# g <- df_documents %>% 
+#     left_join(
+#         dat %>% 
+#             select(abstract, title, year) %>%
+#             filter(!is.na(abstract)) %>%
+#             unique(),
+#         by = c("document" = "title") ) %>%
+#     mutate(year = as.numeric(year)) %>%
+#     filter(year > 2000) %>%
+#     ggplot(aes(year, gamma) )+
+#     geom_jitter(
+#         aes(color = as.factor(topic), fill = as.factor(topic), alpha = gamma),
+#         size = 1, show.legend = FALSE) +
+#     geom_smooth(method = "loess") +
+#     scale_alpha_continuous(range = c(0.1,1)) +
+#     facet_wrap(.~topic, ncol = 5) +
+#     #labs(tag = "B") +
+#     theme_light(base_size = 6) + 
+#     theme(axis.text.x = element_text(size = 4))
+
+g_tl <- dat %>% #filter(year > 2000, year < 2021) %>%
+    group_by(year) %>%
+    tally() %>% 
+    ggplot(aes(year, n)) + geom_line(size = 0.1) + 
+    ggplot2::annotate("rect", xmin = 2000, xmax = 2020, ymin = 0, ymax = 323, alpha = 0.2 ) +
+    labs(y = "Number of papers", x = "Year", tag = "A") + 
+    theme_classic(base_size = 6)
 
 papers_yr <- dat %>%
-    filter(year > 2000) %>%
+    filter(year > 2000, year < 2021) %>%
     group_by(year) %>%
     tally() %>% rename(total_papers = n)
 
 
-g <- df_documents %>% 
+g_prop <- df_documents %>% 
     left_join(
         dat %>% 
             select(abstract, title, year) %>%
@@ -134,7 +150,7 @@ g <- df_documents %>%
             unique(),
         by = c("document" = "title") ) %>%
     mutate(year = as.numeric(year)) %>%
-    filter(year > 2000) %>%
+    filter(year > 2000, year < 2021) %>%
     group_by(document) %>% 
     slice_max(gamma) %>% 
     ungroup() %>% group_by(topic, year) %>%
@@ -142,13 +158,38 @@ g <- df_documents %>%
     left_join(papers_yr) %>%
     mutate(proportion = n / total_papers) %>%
     ggplot(aes(x = year, y = proportion, group = topic)) +
-    geom_line(aes(color = as.factor(topic)), show.legend = FALSE) +
-    labs(y = "Proportion of papers", x = "Year") +
-    facet_wrap(.~topic, ncol = 5) +
-    theme_light(base_size = 6) + 
-    theme(axis.text.x = element_text(size = 4))
-    
-    
+   # geom_col(aes(fill = topic), position = "stack") +
+    geom_line(aes(color = as.factor(topic)), show.legend = FALSE, size = 0.25) +
+    geom_hline(yintercept = 0.04, color = "gray50", size = 0.5, linetype = 2 ) +
+    labs(y = "Proportion of papers", x = "Year", tag = "B") +
+    # facet_wrap(.~topic, ncol = 5) +
+    theme_light(base_size = 6) 
+
+## alternative with stacked lines
+g_stack <-  df_documents %>% 
+    left_join(
+        dat %>% 
+            select(abstract, title, year) %>%
+            filter(!is.na(abstract)) %>%
+            unique(),
+        by = c("document" = "title") ) %>%
+    mutate(year = as.numeric(year)) %>% 
+    filter(year > 2000, year <2021) %>%
+    group_by(topic, year) %>% 
+    summarize(gamma_yr_tp = sum(gamma), .groups = "drop") %>% 
+    group_by(year) %>%
+    mutate(total = sum(gamma_yr_tp)) %>%
+    ungroup() %>% 
+    mutate(proportion = gamma_yr_tp / total,
+           topic = as.factor(topic)) %>%
+    ggplot(aes(x = year, y = proportion, group = topic)) +
+    geom_col(aes(fill = topic), position = "stack", show.legend = FALSE) +
+    labs(y = "Relative proportion of topics", x = "Year", tag = "C") +
+    theme_classic(base_size = 6) 
+
+# + 
+#     theme(axis.text.x = element_text(size = 4))
+
     # ggplot(aes(year, gamma) )+
     # geom_jitter(
     #     aes(color = as.factor(topic), fill = as.factor(topic), alpha = gamma),
@@ -161,12 +202,20 @@ g <- df_documents %>%
     # theme(axis.text.x = element_text(size = 4))
 
 
-g
+(g_tl | g_prop | g_stack) / g_25 + plot_layout(heights = unit(c(1.5,4), "in"))
 
-library(patchwork)
-g1/g
+ggsave(
+    plot = last_plot(),
+    filename = "fig3_topis25.png", path = "paper/figures",
+    device = "png", width = unit(6.5,"in"), height = unit(6.5,"in"), 
+    dpi = 500)
 
-ggsave("fig3_topis25_timeline2.png", device = "png", width = unit(6,"in"), height = unit(4,"in"))
+## useful for writing:
+gg <- plotly::ggplotly(g_prop)
+gg
+
+plotly::highlight(gg, dynamic = TRUE, on = "plotly_hover")
+
 
 #### qualitative data ####
 
